9cf6f7a570e3db1387a0d87d3d7738712c069b29
= <=>
2025-01-03 14:32:25 +0300

    readme

--- watch.py
+++ watch.py
@@ -23,12 +23,12 @@ def parse(html: str)-> str:
         str: _Shorter url for the page_
     """
     
-    pattern = r"(?P<url>(?:http|https)://(www)?\.youtube\.com/embed/[a-z0-9]+)"
+    pattern = r"(?P<url>(?:http|https)://(www\.)?youtube\.com/embed/[a-z0-9]+)"
 
     url = result.group('url') if (result := search(pattern=pattern, string=html, flags=IGNORECASE)) else None
-    return shorten(html=url)
+    return shorten(html=url) if url else None
 def main()-> None:
-    long='<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    long='<iframe width="560" height="315" src="http://youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 
     print(parse(html=long))
 
